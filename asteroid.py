3ed6db06ba75b0df2f75ff6cb50313cb13725210
CaioVictor <caiorbcavalcante@gmail.com>
2025-04-19 14:36:54 -0300

    Asteroids prontos

---
+++ asteroid.py
@@ -0,0 +1,12 @@
+from player import *
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)  # Parent class will set position and radius
+
+    def draw(self, screen):
+        # Use the already defined position and radius
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 import pygame
 
 print("Starting Asteroids!")
 print(f"Screen width: {SCREEN_WIDTH}")
 print(f"Screen height: {SCREEN_HEIGHT}")
 
-x = SCREEN_WIDTH / 2
-y = SCREEN_HEIGHT / 2
-player = Player(x, y)
+
 
 
 def main():
@@ -18,7 +18,16 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.container = (updatable,drawable)
+    Player.containers = (updatable,drawable)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    asteroidfield = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -27,7 +36,9 @@ def main():
             
         screen.fill("black")
         updatable.update(dt)
-        drawable.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
